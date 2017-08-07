1f03155bde2354ccf8e725d09464303d00e0075b
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-08-07 10:40:27 -0600

    Added secret key base for production

--- Gemfile
+++ Gemfile
@@ -39,5 +39,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'dotenv-rails', '~> 2.2', '>= 2.2.1'
 
 ruby '2.4.0'
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module FreelanceCampProposal
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
